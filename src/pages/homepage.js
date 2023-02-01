5a704cf98f7d53cbe44ced5fbe95b5a40933079b
Kunal Kishore <kunalkishore1807@gmail.com>
2023-02-01 16:38:58 +0530

    Production Showcase completed

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import HeroSection from "../components/HeroSection";
 import Header from "../components/common/Header";
+import ProductShowCase from "../components/ProductionShowCase";
 
 const Homepage = () => {
   return (
     <React.Fragment>
       <Header />
       <HeroSection />
+      <ProductShowCase/>
     </React.Fragment>
   );
 };
