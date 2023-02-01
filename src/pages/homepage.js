d03fbecf77c45b9fdf078342bd268d03b7046125
Kunal Kishore <kunalkishore1807@gmail.com>
2023-02-01 13:28:00 +0530

    heroSection completed

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,10 +1,14 @@
-import React from 'react'
-import HeroSection from '../components/HeroSection';
+import React from "react";
+import HeroSection from "../components/HeroSection";
+import Header from "../components/common/Header";
 
 const Homepage = () => {
   return (
-    <HeroSection/>
-  )
-}
+    <React.Fragment>
+      <Header />
+      <HeroSection />
+    </React.Fragment>
+  );
+};
 
 export default Homepage;
